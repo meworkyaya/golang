db11ab8b2b0fc7307d0011693636dc0f9e4cec4f
tryit <amstudio777@gmail.com>
2015-11-04 20:42:59 +0200

    init

---
+++ proxyload/proxyload.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"log"
+)
+
+func main() {
+	
+	fmt.Println("Proxyload running")
+	
+	// data := loadUrl( "test")
+	// fmt.Printf( "%s", data )	
+	
+	var urls [2] string
+	urls[0] = "http://example.com"
+	urls[1] = "http://amstudio-design.com"
+	
+	loadUrlList( urls )
+	
+}
+
+
+/**
+* test loading url function
+*/
+func loadUrl( url string ) string {
+	resp, err := http.Get( url )
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+	
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	res := string( body )
+	
+	return res
+	
+}
+
+
+/**
+* load list of urls
+*/
+func loadUrlList( urls [2] string)  {
+	len := 2
+	for i := 0; i < len; i++ {
+		res := loadUrl( urls[ i ])
+		fmt.Printf( "%s", res )
+		
+		res = ""
+		
+		fmt.Println("")
+		fmt.Println("")
+		fmt.Println("")
+	}	
+}
