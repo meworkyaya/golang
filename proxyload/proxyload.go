acfbd6d7f3a912a3f05017b2457d93f8063df562
tryit <amstudio777@gmail.com>
2016-01-11 13:23:34 +0200

    proxy update

--- proxyload/proxyload.go
+++ proxyload/proxyload.go
@@ -3,62 +3,60 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"net/http"
 	"log"
+	"net/http"
 )
 
 func main() {
-	
+
 	fmt.Println("Proxyload running")
-	
+
 	// data := loadUrl( "test")
-	// fmt.Printf( "%s", data )	
-	
-	var urls [2] string
+	// fmt.Printf( "%s", data )
+
+	var urls [2]string
 	urls[0] = "http://example.com"
 	urls[1] = "http://amstudio-design.com"
-	
-	loadUrlList( urls )
-	
-}
 
+	loadUrlList(urls)
+
+}
 
 /**
 * test loading url function
-*/
-func loadUrl( url string ) string {
-	resp, err := http.Get( url )
+ */
+func loadUrl(url string) string {
+	resp, err := http.Get(url)
 	if err != nil {
 		// handle error
 		log.Fatal(err)
 	}
-	
+
 	body, err := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	res := string( body )
-	
-	return res
-	
-}
 
+	res := string(body)
+
+	return res
+
+}
 
 /**
 * load list of urls
-*/
-func loadUrlList( urls [2] string)  {
+ */
+func loadUrlList(urls [2]string) {
 	len := 2
 	for i := 0; i < len; i++ {
-		res := loadUrl( urls[ i ])
-		fmt.Printf( "%s", res )
-		
+		res := loadUrl(urls[i])
+		fmt.Printf("%s", res)
+
 		res = ""
-		
+
 		fmt.Println("")
 		fmt.Println("")
 		fmt.Println("")
-	}	
+	}
 }
